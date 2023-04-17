7bb021f29e50c128d5b6ccf278cbf26eb1133b6e
AArkNite <127753203+AArkNite@users.noreply.github.com>
2023-04-17 13:57:24 +0200

    He camviat el script del tanc1
    e

--- Tanc1.gd
+++ Tanc1.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 var moviment := Vector2.ZERO
-
+var velocitat := 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,7 +12,8 @@ func _ready():
 func _process(delta):
 	pass
 
-func avança():
-	
+func avanca():
+	moviment.x += velocitat
+	moviment = $KinematicBody2D.move_and_slide(moviment)
 	
 	
