7587111af6de2d5adb807f26b5572aa09374995c
AlBlinder <127503733+AlBlinder@users.noreply.github.com>
2023-04-28 13:07:11 +0200

    Mapa i enemics
    El mapa està perfeccionat i les posicions dels enemics estan arreglades.

--- Tanc1.gd
+++ Tanc1.gd
@@ -5,17 +5,17 @@ var moviment := Vector2(1,1)
 var velocitat := 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	position = Vector2(-29, 96)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-func rota():
+func rota(angle):
 	if $KinematicBody2D.rotation_degrees > 360:
 		$KinematicBody2D.rotation_degrees -= 360
-	$KinematicBody2D.rotation_degrees += 1
+	$KinematicBody2D.rotation_degrees += angle
 
 func avanca():
 	moviment = $KinematicBody2D.move_and_slide(moviment)
