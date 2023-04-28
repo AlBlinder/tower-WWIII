1a8539889b11f5690674ef55c782710d67d75c0d
AlBlinder <127503733+AlBlinder@users.noreply.github.com>
2023-04-28 13:13:58 +0200

    Detalls

--- Codi_principal.gd
+++ Codi_principal.gd
@@ -14,6 +14,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	$Joc/Enemics/Tanc1.avanca()
-	$Joc/Enemics/Tanc1.rota(0.4)
+	#$Joc/Enemics/Tanc1.rota(0.4)
 	
 
