3424ffae1f102bbd8c9eb0e9ce156e2ca7c86598
AArkNite <127753203+AArkNite@users.noreply.github.com>
2023-05-02 06:42:28 +0200

    Arreglat menu
    he post soroll al menu i ara ja van els credits

--- Canviador_escenes.gd
+++ Canviador_escenes.gd
@@ -1,29 +1,35 @@
 extends Node
 
+
+
+
+
 func _ready():
 	get_node("Menu/MarginContainer/VBoxContainer/juga").connect("pressed", self, "clicar_juga")
 	get_node("Menu/MarginContainer/VBoxContainer/surt").connect("pressed", self, "clicar_surt")
 	get_node("Menu/MarginContainer/VBoxContainer/credits").connect("pressed", self, "clicar_credits")
 	get_node("Credits/MarginContainer/VBoxContainer/torna_menu").connect("pressed", self, "clicar_menu")
-	print()
-	
+	get_node("Credits").visible = false
+
+
 func clicar_surt():
+	$clicar_boto.play()
 	get_tree().quit()
 
-
 func clicar_juga():
+	$clicar_boto.play() 
 	get_node("Menu").queue_free()
 	get_node("Credits").queue_free()
-	var escena_joc = load("res://Joc.tscn").instance()
+	var escena_joc = load("res://Joc.tscn").instance()	
 	add_child(escena_joc)
 
 func clicar_credits():
-	get_node("Menu").queue_free()
-	var escena_credits = load("res://Credits.tscn").instance()
-	add_child(escena_credits)
+	$clicar_boto.play() 
+	get_node("Menu").visible = false
+	get_node("Credits").visible = true
 	
 func clicar_menu():
-	get_node("Credits").queue_free()
-	var escena_menu = load("res://Menu.tscn").instance()
-	add_child(escena_menu)
+	$clicar_boto.play()
+	get_node("Menu").visible = true
+	get_node("Credits").visible = false
 	
---
+++ Menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+func _on_surt_mouse_entered():
+	$soroll_boto.play()
+	
+func _on_credits_mouse_entered():
+	$soroll_boto.play() 
+
+func _on_juga_mouse_entered():
+	$soroll_boto.play() 
+
