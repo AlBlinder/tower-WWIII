3ac83f299aae4cf14e6795d6492500521ff778e2
llears123 <127503216+llears123@users.noreply.github.com>
2023-05-05 13:01:05 +0200

    Torres a enemics
    Les torres segueixen els enemics

--- Torretes/Torretes.gd
+++ Torretes/Torretes.gd
@@ -1,16 +1,21 @@
 extends Node2D
 var enemic_llista = []
 var built = false
-
-
+func _ready():
+	if built:
+		pass
+	
 
 func _process(delta):
 	gira()
 	
 	
 func gira():
-	var posicio_enemic = get_global_mouse_position()
-	get_node("Torreta").look_at(posicio_enemic)
+	if enemic_llista.size() == 0:
+		return
+	
+	var posicio_enemic = enemic_llista[0].global_position
+	$Torreta.look_at(posicio_enemic)
 	rotation_degrees += 90
 	
 func _on_A_body_entered(body):
