5cd77c093b585355085c1cceb317ed43bf6b5ddd
AlBlinder <127503733+AlBlinder@users.noreply.github.com>
2023-05-05 13:08:05 +0200

    Velocitat canviada del tanc 1

--- Tanc1.gd
+++ Tanc1.gd
@@ -2,7 +2,7 @@ extends PathFollow2D
 
 
 var moviment := Vector2(1,1)
-var velocitat := 50
+var velocitat := 100
 # Called when the node enters the scene tree for the first time.
 func _ready():
 #	position = Vector2(-29, 96)
