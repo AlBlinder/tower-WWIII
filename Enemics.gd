c13389626d02443dc3edfecb8dcf685dc943daa6
AlBlinder <127503733+AlBlinder@users.noreply.github.com>
2023-05-05 13:06:28 +0200

    Arreglades les instàncies dels tancs

--- Enemics.gd
+++ Enemics.gd
@@ -1,16 +1,13 @@
 extends Path2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var tancs = [preload("res://Tanc1.tscn"), preload("res://Tanc2.tscn"), preload("res://Tanc3.tscn")]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	crea_enemic(1)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func crea_enemic(tipus):
+	var enemic_nou = tancs[tipus-1].instance()
+	add_child(enemic_nou)
