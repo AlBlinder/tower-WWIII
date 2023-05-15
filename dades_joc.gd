147285a548d39b0f1ed18e94c71f17a1f57a9b64
AArkNite <127753203+AArkNite@users.noreply.github.com>
2023-05-15 13:44:02 +0200

    canvis

--- dades_joc.gd
+++ dades_joc.gd
@@ -1,22 +1,19 @@
 extends Node
 
 
-var diners = 1000
+var diners = 100
 var vida_base = 100
 var dades_torres = {
 	"T1": {
 		"mal": 30,
-		"Area": 350,
 		"temps_recarrega": 2,
 		"preu" : 500},
 	"T2": {
 		"mal": 7,
-		"Area": 100,
 		"temps_recarrega": 0.4,
 		"preu" : 600},
 	"T3": {
 		"mal": 50,
-		"Area": 500,
 		"temps_recarrega": 3,
 		"preu" : 800}
 }
